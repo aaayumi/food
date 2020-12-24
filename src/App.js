d919822df6d192fa889baa3ee4bbc7b3f9d0d7a5
ayumi saito <ayumisaito@ayumis-MBP.fritz.box>
2020-12-24 16:55:36 +0100

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState();
+  const API_KEY = process.env.REACT_APP_API_KEY
+
+  useEffect(() => {
+    let mounted = true;
+    fetch('https://api.spoonacular.com/food/ingredients/search?apiKey=${API_KEY}&query=apple')
+        .then( data => data.json())
+        .then(item => {
+          if(mounted) {
+            setData(item)
+          }
+        })
+    return () => mounted = false;
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
