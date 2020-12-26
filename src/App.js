e860457c460f9ddcd518f5a49a54f24d5370e5f3
ayumi saito <ayumisaito@ayumis-MBP.fritz.box>
2020-12-26 15:15:41 +0100

    add ingredient search

--- src/App.js
+++ src/App.js
@@ -3,22 +3,43 @@ import './App.css';
 
 function App() {
   const [data, setData] = useState();
+  const [search, setSearch] = useState();
   const API_KEY = process.env.REACT_APP_API_KEY
 
-  useEffect(() => {
-    let mounted = true;
-    fetch('https://api.spoonacular.com/food/ingredients/search?apiKey=${API_KEY}&query=apple')
-        .then( data => data.json())
-        .then(item => {
-          if(mounted) {
-            setData(item)
-          }
-        })
-    return () => mounted = false;
-  }, [])
+
+  // useEffect(() => {
+  //   let mounted = true;
+  //   fetch(`https://api.spoonacular.com/food/ingredients/search?apiKey=${API_KEY}&query=apple`)
+  //       .then( data => data.json())
+  //       .then(item => {
+  //         if(mounted) {
+  //           setData(item)
+  //         }
+  //       })
+  //   return () => mounted = false;
+  // }, [])
+
+
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      fetch(`https://api.spoonacular.com/food/ingredients/search?apiKey=${API_KEY}&query=${search}`)
+          .then(data => data.json())
+          .then(item => {
+              setData(item)
+          })
+    }
 
   return (
     <div className="App">
+        <form onSubmit={handleSubmit}>
+        <input type={`text`} onChange={(e) => setSearch(e.target.value)} />
+        </form>
+
+        {data &&
+            data.results.map((result, i) =>
+                <li key={i}>{result.name}</li>)
+        }
     </div>
   );
 }
